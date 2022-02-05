ae35d8d2e1d362e5b9dec5875da2e851b119ea3e
aguspitarque <pitarqueagustin@gmail.com>
2022-02-04 23:32:59 -0300

    config app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,30 @@
 require('./bootstrap');
+
+//import vue
+import vue from 'vue';
+window.Vue = vue;
+
+//Principal component
+import App from './components/App';
+
+//axios
+import VueAxios from 'vue-axios';
+import axios from 'axios';
+
+//vue-router
+import VueRouter from "vue-router";
+import { routes } from './routes';
+import Vue from 'vue';
+Vue.use(VueRouter);
+Vue.use(VueAxios,axios);
+
+const router = new VueRouter({
+   mode: 'history',
+   routes: routes
+});
+
+const app = new Vue({
+    el: '#app',
+    router: router,
+    render: h => h(App)
+});
