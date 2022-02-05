84801a870bd4e1e89f650f1842b3062923d2b158
aguspitarque <pitarqueagustin@gmail.com>
2022-02-05 17:18:36 -0300

    fixed controller class

--- routes/api.php
+++ routes/api.php
@@ -18,4 +18,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::resource('task', \App\Models\Task::class);
+Route::resource('task', \App\Http\Controllers\TaskController::class);
