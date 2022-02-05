24bfd3586464348e77e770b4aef6ea9d4b841807
aguspitarque <pitarqueagustin@gmail.com>
2022-02-05 17:19:51 -0300

    add axios-middleware

--- resources/js/app.js
+++ resources/js/app.js
@@ -11,6 +11,24 @@ import App from './components/App';
 import VueAxios from 'vue-axios';
 import axios from 'axios';
 
+//axios-middleware
+import { Service } from 'axios-middleware';
+
+const service = new Service(axios);
+
+service.register({
+    onResponseError(error){
+        if(error.response.status == 422
+        && error.response.statusText == "Unprocessable Content"){
+            let data = JSON.parse(error.response.data)
+            data = Array.from(data.errors.title)
+            data.forEach(err =>{
+                alert('danger', err)
+            })
+        }
+    }
+});
+
 //vue-router
 import VueRouter from "vue-router";
 import { routes } from './routes';
@@ -28,3 +46,25 @@ const app = new Vue({
     router: router,
     render: h => h(App)
 });
+
+const alert = (theme, message)=>{
+
+    let title = ""
+
+    switch(theme){
+        case "danger":
+           title = "Error"
+        break
+        default:
+            title = "Warning"
+        break
+    }
+
+    document.querySelector('body').innerHTML +=
+        `
+            <div class="alert alert-${theme} alert-dismissible fade show" role="alert">
+              <strong>${title}</strong> ${message}
+              <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+            </div>
+        `
+}
